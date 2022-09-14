a15269d05a8f7d4d2843fbe76239dbe68cc30a2c
Glib Lakeev <glakeev@gmail.com>
2022-09-14 13:08:48 -0400

    trying to get page switch to work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import "./styles.css";
+import "./pageSwitch";
 
+//event listeners for menu
+const homeButton = document.getElementsByClassName(".homeButton"[0]);
+homeButton.addEventListener(`click`, pageSwitch.homeSwitch());
+
+const menuButton = document.getElementsByClassName(".menuButton"[0]);
+menuButton.addEventListener(`click`, pageSwitch.menuSwitch());
+
+const contactButton = document.getElementsByClassName(".contactButton"[0]);
+contactButton.addEventListener(`click`, pageSwitch.contactSwitch());
+
+//page body:
 const body = document.getElementById("content");
 
 const bodyDiv = document.createElement("div");
@@ -13,4 +25,6 @@ const text = document.createTextNode("This-a pizza place, it's-a so good. She ma
 bodyCopy.appendChild(text);
 bodyDiv.appendChild(bodyCopy);
 body.appendChild(bodyDiv);
+
+
 
--- src/pageSwitch.js
+++ src/pageSwitch.js
@@ -1,3 +1,75 @@
 class pageSwitch{
+    constructor(){
+
+    }
+
+    homeSwitch(){
+        console.log("homeswitch");
+
+        //remove existing
+        var el = document.getElementById("content");
+        while ( el.firstChild ) el.removeChild( el.firstChild );
+
+
+        //add more
+        const body = document.getElementById("content");
+
+        const bodyDiv = document.createElement("div");
+        bodyDiv.classList.add("bodyDiv");
+
+        const bodyCopy = document.createElement("p");
+        bodyCopy.classList.add("bodyText");
+
+        const text = document.createTextNode("This-a pizza place, it's-a so good. She make-a-da cheese a-melt in-a your mouth-a.");
+
+        bodyCopy.appendChild(text);
+        bodyDiv.appendChild(bodyCopy);
+        body.appendChild(bodyDiv);
+    }
+
+    menuSwitch(){
+        console.log("menuswitch");
+
+        //remove existing
+        var el = document.getElementById("content");
+        while ( el.firstChild ) el.removeChild( el.firstChild );
+
+        const body = document.getElementById("content");
+
+        const bodyDiv = document.createElement("div");
+        bodyDiv.classList.add("bodyDiv");
+
+        const bodyCopy = document.createElement("p");
+        bodyCopy.classList.add("bodyText");
+
+        const text = document.createTextNode("This is-a da pizza menu");
+
+        bodyCopy.appendChild(text);
+        bodyDiv.appendChild(bodyCopy);
+        body.appendChild(bodyDiv);
+    }
+
+    contactSwitch(){
+        console.log("contactswitch");
+
+        //remove existing
+        var el = document.getElementById("content");
+        while ( el.firstChild ) el.removeChild( el.firstChild );
+
+        const body = document.getElementById("content");
+
+        const bodyDiv = document.createElement("div");
+        bodyDiv.classList.add("bodyDiv");
+
+        const bodyCopy = document.createElement("p");
+        bodyCopy.classList.add("bodyText");
+
+        const text = document.createTextNode("Contact-a da chef");
+
+        bodyCopy.appendChild(text);
+        bodyDiv.appendChild(bodyCopy);
+        body.appendChild(bodyDiv);
+    }
+
     
-}
+};
