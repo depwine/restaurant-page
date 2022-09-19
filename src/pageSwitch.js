742c6b6736bf81f0f142280b7379d01d61cdf371
Glib Lakeev <glakeev@gmail.com>
2022-09-19 13:05:40 -0400

    added some placeholder. it does the thing, shipping

--- src/pageSwitch.js
+++ src/pageSwitch.js
@@ -42,18 +42,77 @@ export var menuSwitch =  function(){
     var el = document.getElementById("content");
     while ( el.firstChild ) el.removeChild( el.firstChild );
 
+    // create new
     const body = document.getElementById("content");
 
     const bodyDiv = document.createElement("div");
     bodyDiv.classList.add("bodyDiv");
 
+    
+    const floatCenter = document.createElement("div");
+    floatCenter.classList.add("floatCenter");
+
+    
+    //add some images
+    const rectOne = document.createElement("div");
+    rectOne.classList.add("rectOne");
+
+    const rectTwo = document.createElement("div");
+    rectTwo.classList.add("rectTwo");
+
+    const rectThree = document.createElement("div");
+    rectThree.classList.add("rectThree");
+
+    const rectFour = document.createElement("div");
+    rectFour.classList.add("rectFour");
+
     const bodyCopy = document.createElement("p");
     bodyCopy.classList.add("bodyText");
+
+    const menuOne = document.createElement("p");
+    menuOne.classList.add("menuOne");
+
+    const menuTwo = document.createElement("p");
+    menuTwo.classList.add("menuTwo");
+
+    const menuThree = document.createElement("p");
+    menuThree.classList.add("menuThree");
+
+    const menuFour = document.createElement("p");
+    menuFour.classList.add("menuFour");
+
+    const menuOneText = document.createTextNode("Menu Item #1");
+    const menuTwoText = document.createTextNode("Menu Item #2");
+    const menuThreeText = document.createTextNode("Menu Item #3");
+    const menuFourText = document.createTextNode("Menu Item #4");
+
 
     const text = document.createTextNode("This is-a da pizza menu");
 
     bodyCopy.appendChild(text);
-    bodyDiv.appendChild(bodyCopy);
+
+    menuOne.appendChild(menuOneText);
+    menuTwo.appendChild(menuTwoText);
+    menuThree.appendChild(menuThreeText);
+    menuFour.appendChild(menuFourText);
+
+
+
+    floatCenter.appendChild(bodyCopy);
+
+    floatCenter.appendChild(rectOne);
+    floatCenter.appendChild(menuOne);
+
+    floatCenter.appendChild(rectTwo);
+    floatCenter.appendChild(menuTwo);
+
+    floatCenter.appendChild(rectThree);
+    floatCenter.appendChild(menuThree);
+
+    floatCenter.appendChild(rectFour);
+    floatCenter.appendChild(menuFour);
+    
+    bodyDiv.appendChild(floatCenter);
     body.appendChild(bodyDiv);
 }
 
